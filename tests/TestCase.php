9de6032f5a3e6ce135a6e95bfc0eaefe1e2caac3
Mateus Guimarães <mateus.jatene@gmail.com>
2017-05-24 15:41:29 -0300

    load package providers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,12 @@
 
 namespace Tests;
 
+use Codecasts\Auth\JWT\ServiceProvider;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-
+    public function getPackageProviders()
+    {
+        return [ServiceProvider::class, AuthService::class];
+    }
 }
